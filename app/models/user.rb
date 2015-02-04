7d82a64ae709df5bad9604abce2a6f7787ae4f44
Thibaut Roche <thibaut.roche.perso@gmail.com>
2015-02-04 13:14:29 -0500

    [A]Set admin when there is no one at user creation

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  before_create :check_admin
+
+  private
+
+  def check_admin
+    self.admin = true if User.where(admin: true).count == 0
+  end
 end
